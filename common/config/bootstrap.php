96f4303ffd50954a0a2f83d2a86abdd9a37dff5e
Adriana <adriana.morsucci@gmail.com>
2022-06-03 13:59:11 -0300

    Envio Common

---
+++ common/config/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+//Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+//Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@apirest', dirname(dirname(__DIR__)) . '/apirest');
+Yii::setAlias('@api1', dirname(dirname(__DIR__)) . '/api1');
